a3b80bcdd74eb8ae9a097e3bd16a53ad96559632
Pawel Psztyc <jarrodek@gmail.com>
2019-04-02 23:20:15 -0700

    Update: Updating styles to remove reference to `@apply` function

--- form-action-bar.js
+++ form-action-bar.js
@@ -13,7 +13,6 @@ the License.
 */
 import {PolymerElement} from '../../@polymer/polymer/polymer-element.js';
 import {html} from '../../@polymer/polymer/lib/utils/html-tag.js';
-import '../../@polymer/paper-styles/shadow.js';
 /**
  * The `<form-action-bar>` renders an action buttons and status information in the bottom of the form.
  *
@@ -62,19 +61,19 @@ import '../../@polymer/paper-styles/shadow.js';
  */
 class FormActionBar extends PolymerElement {
   static get template() {
-    return html`
-    <style include="paper-material-shared-styles"></style>
-    <style>
+    return html`<style>
     :host {
       background-color: var(--form-action-bar-background, #fff);
-      @apply --layout-horizontal-reverse;
-      @apply --layout-center;
+      display: flex;
+      flex-direction: row-reverse;
+      align-items: center;
       width: 100%;
       height: var(--form-action-bar-height, 64px);
       position: fixed;
       bottom: 0px;
       left: 0;
-      @apply --shadow-none;
+      box-shadow: none;
+      transition: box-shadow 0.28s cubic-bezier(0.4, 0, 0.2, 1);
     }
 
     :host ::slotted(*) {
@@ -82,43 +81,59 @@ class FormActionBar extends PolymerElement {
     }
 
     :host([elevation="1"]) {
-      @apply --shadow-elevation-2dp;
+      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+                  0 1px 5px 0 rgba(0, 0, 0, 0.12),
+                  0 3px 1px -2px rgba(0, 0, 0, 0.2);
     }
 
     :host([elevation="2"]) {
-      @apply --shadow-elevation-3dp;
+      box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
+                  0 1px 8px 0 rgba(0, 0, 0, 0.12),
+                  0 3px 3px -2px rgba(0, 0, 0, 0.4);
     }
 
     :host([elevation="3"]) {
-      @apply --shadow-elevation-4dp;
+      box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+                  0 1px 10px 0 rgba(0, 0, 0, 0.12),
+                  0 2px 4px -1px rgba(0, 0, 0, 0.4);
     }
 
     :host([elevation="4"]) {
-      @apply --shadow-elevation-6dp;
+      box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14),
+                  0 1px 18px 0 rgba(0, 0, 0, 0.12),
+                  0 3px 5px -1px rgba(0, 0, 0, 0.4);
     }
 
     :host([elevation="5"]) {
-      @apply --shadow-elevation-8dp;
+      box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
+                  0 3px 14px 2px rgba(0, 0, 0, 0.12),
+                  0 5px 5px -3px rgba(0, 0, 0, 0.4);
     }
 
     :host([elevation="6"]) {
-      @apply --shadow-elevation-12dp;
+      box-shadow: 0 12px 16px 1px rgba(0, 0, 0, 0.14),
+                  0 4px 22px 3px rgba(0, 0, 0, 0.12),
+                  0 6px 7px -4px rgba(0, 0, 0, 0.4);
     }
 
     :host([elevation="7"]) {
-      @apply --shadow-elevation-16dp;
+      box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14),
+                  0  6px 30px 5px rgba(0, 0, 0, 0.12),
+                  0  8px 10px -5px rgba(0, 0, 0, 0.4);
     }
 
     :host([elevation="8"]) {
-      @apply --shadow-elevation-24dp;
+      box-shadow: 0 24px 38px 3px rgba(0, 0, 0, 0.14),
+                  0 9px 46px 8px rgba(0, 0, 0, 0.12),
+                  0 11px 15px -7px rgba(0, 0, 0, 0.4);
     }
 
     .content {
-      @apply --layout-flex;
-      @apply --layout-end-justified;
-      @apply --layout-horizontal;
-
-      @apply --form-action-bar-content;
+      display: flex;
+      flex: 1;
+      flex-basis: 0.000000001px;
+      justify-content: flex-end;
+      flex-direction: row;
     }
     </style>
     <div class="content">
